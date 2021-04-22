87f4c9ee448a06ccb55a83ecb72a4a45f8369a47
davibelini <dev.dado.belini@gmail.com>
2021-04-21 21:34:52 -0300

    created first components

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,18 @@
 <script>
-	export let name;
+	import Header from "./components/Header.svelte"
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Header></Header>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	:global(html) {
+		box-sizing: border-box;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	:global(*), :global(*):before, :global(*):after {
+		box-sizing: inherit;
 	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+  import LinkArea from "./LinkArea.svelte"
+</script>
+
+<main>
+  <header class="top">
+    <LinkArea></LinkArea>
+  </header>
+</main>
+
+<style>
+  .top {
+    background-color: red;
+  }
+</style>
